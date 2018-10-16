60bbc70318261a6dfc76dbdb4d4b68ca29bce9f2
avivko <aviv.korman@gmail.com>
2018-10-17 00:50:21 +0200

    second commit with some examples

---
+++ practice2.py
@@ -0,0 +1,13 @@
+import random
+
+listA = ["apple", "cheese", "seitan"]
+listB = ["boy", "girl", "queer"]
+listC = ["police officer", "artist", "pirate"]
+
+story = {"food": listA, "people": listB, "jobs": listC}
+
+print(story["food"])
+
+givenPhrase = input("please write a sentence to get it reversed and in numbers (a-e)")
+crazyDict = {"a": 1, "b": 2, "c": 3, "d": 4, "e": 5}
+characterList = list(givenPhrase)
