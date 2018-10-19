4f57aa055efbd89ef1a5241889f58619f69725ca
avivko <aviv.korman@gmail.com>
2018-10-19 16:17:57 +0200

    commit with 3.3 done (second page done)

--- blatt3.py
+++ blatt3.py
@@ -1,7 +1,7 @@
 from random import *
 
 
-def create_random_binary_matrix(m, n):
+def rand(m, n):
     matrix = []
     for i in range(m):
         row = []
@@ -10,5 +10,25 @@ def create_random_binary_matrix(m, n):
         matrix.append(row)
     return matrix
 
+
+def identity(n):
+    matrix = []
+    for i in range(n):
+        row = []
+        for j in range(n):
+            if i == j:
+                row.append(1)
+            else:
+                row.append(0)
+        matrix.append(row)
+    return matrix
+
+
+def dot(v1,v2):
+    assert (len(v1) == len(v2)), "Vectors have different dimensions!"
+    dotproduct = 0
+    for i in range(len(v1)):
+        dotproduct += v1[i] * v2[i]
+    return dotproduct
 
 
